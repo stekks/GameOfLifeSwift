b952fd0f105b367e9f6b82478fc8db2ed78a6937
Jan Broeze <apple@Computer-van-Jan-2.local>
2014-10-22 20:42:42 +0200

    Grid size fixes on 6 to prevent crash. Experiment with gestures to detect a click on a cell in the grid (not working yet).

--- GameOfLifeSwift/SecondViewController.swift
+++ GameOfLifeSwift/SecondViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIGestureRecognizerDelegate {
 
     enum GameState {
         case Started, Stopped
@@ -33,10 +33,7 @@ class SecondViewController: UIViewController {
         
         // create grid of views(?) depending on size of main view
         createGrid()
-        // create timer according to interval parameter when start button is pressed
         // change grid in LifeLogic according to the grid on the screen
-        // call LifeLogic on timer events
-        // change grid on the screen according to next generation of LifeLogic grid
     }
     
     override func didReceiveMemoryWarning() {
@@ -64,13 +61,13 @@ class SecondViewController: UIViewController {
     
     func createGrid()
     {
-        // draw grid with lines (maybe not needed if 'smart' images are chosen)
+        var imageTag = 0
         
-        // fill grid with (scaled!) UIImageViews of 'dead' cells. (dead cells are white, life cells are black?)
-        // connect on-click eventhandler on over UIImageView
-        /*
+        GridSize = 6 // temp fix for crash on 7th interation of for loop below
+        
+        // fill grid with (scaled!) UIImageViews of 'dead' cells.
         for x in 0..<GridSize {
-            for y in 0..<GridSize {
+            for y in 0..<GridSize{
                 var imageView = UIImageView(
                     frame: CGRectMake(
                         CGFloat(x * gridElementSize + xOffset),
@@ -79,16 +76,18 @@ class SecondViewController: UIViewController {
                         CGFloat(gridElementSize))
                 )
                 println("\(x) \(y)")
-                imageView.contentMode = UIViewContentMode.ScaleAspectFit;
+                imageView.contentMode = .ScaleAspectFit;
                 var image = UIImage(named: "cell_white.png")
                 imageView.image = image
-                self.view.addSubview(imageView)   <- crashes on 7th iteration.. why?
-                imageView.tag = Int((x * 100) + y)
-                // imageView.touchesEnded(<#touches: NSSet#>, withEvent: <#UIEvent#>)
+                self.view.addSubview(imageView)   // <- crashes on 7th iteration.. why?
+                imageView.tag = imageTag
+                
+                // add click event on image view
+                let recognizer = UITapGestureRecognizer(target: self, action:Selector("handleTap:"))
+                recognizer.delegate = self
+                imageView.addGestureRecognizer(recognizer)
             }
-        } 
-        */
-        // fill tag value of every UIImageView: x+(y*size) so we can deduce the position in the 2d-array
+        }
     }
     
     // called by timer
@@ -96,6 +95,11 @@ class SecondViewController: UIViewController {
     {
         // LifeLogic.calculateNextGeneration
         println("timerinterval fired")        
+    }
+    
+    func handleTap(recognizer: UITapGestureRecognizer) {
+        // check if a/which cell is clicked
+        println(recognizer.view!.tag)
     }
 
 }
